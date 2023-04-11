135f284e58841ca6c6d6d98286af6ea488093dfd
marimalll <123314557+marimalll@users.noreply.github.com>
2023-04-11 21:52:44 +0500

    adding about us page

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    app.run(port=8880, host='127.0.0.1')
--- test_elvina.py
+++ test_elvina.py
@@ -2,6 +2,9 @@ import flask
 from flask import Flask
 from flask import Flask, render_template, session
 from flask import redirect, request, make_response
+from flask import make_response
+
+
 
 app = Flask(__name__)
 
@@ -11,6 +14,16 @@ app = Flask(__name__)
 def index():
     data = 'Простое заметки - это простой и удобный инструмент для создания собственных заметок. Вы можете легко создавать, редактировать и удалять заметки в любое время. Этот инструмент также позволяет создавать списки задач и отмечать их по мере выполнения.'
     return render_template('index.html', data=data.split('. '))
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/register')
+def register():
+    return 'регистрация'
 
 
 if __name__ == '__main__':
