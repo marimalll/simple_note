fc9215d61bb2c88bb8a4d2d7a327f3a2e90f709b
marimalll <123314557+marimalll@users.noreply.github.com>
2023-04-12 16:13:54 +0500

    adding register and login

--- test_elvina.py
+++ test_elvina.py
@@ -3,10 +3,13 @@ from flask import Flask
 from flask import Flask, render_template, session
 from flask import redirect, request, make_response
 from flask import make_response
-
-
+from data import db_session
+from data.users import User
+from forms.login import LoginForm
+from forms.user import RegisterForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'ourprojectsecretkey'
 
 
 @app.route('/')
@@ -21,10 +24,37 @@ def about():
     return render_template('about.html')
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return 'регистрация'
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', form=form, message='Пароли не совпадают')
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(name=form.name.data,
+                    email = form.email.data,
+                    about = form.about.data)
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', form=form)
+
+
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', title='Авторизация', form=form)
 
 
 if __name__ == '__main__':
+    db_session.global_init("db/simple_note.db")
     app.run(port=8080, host='127.0.0.1')
